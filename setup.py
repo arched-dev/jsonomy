a5f26ad081adc8fe517c2afc26f6ece56828a3d1
lewis <lewis.morris@gmail.com>
2023-08-04 11:26:47 +0100

    Initial Version

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
+
+import os
 from setuptools import setup, find_packages
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
+    long_description = f.read()
 
 setup(
     name='jsonomy',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(exclude=["tests.*", "tests"]),
     url='https://github.com/lewis-morris/jsonomy',
     license='MIT',
@@ -10,6 +15,8 @@ setup(
     author_email='lewis.morris@gmail.com',
     description="Basic JSON parser, that converts APIs JSON output to be more pythonic, including camel case to "
                 "snake case conversion, and converting timestamps to datetime objects.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
